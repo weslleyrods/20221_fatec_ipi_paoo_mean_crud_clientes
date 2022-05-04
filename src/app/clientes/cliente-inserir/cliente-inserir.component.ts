cb523dbad57431e0bf58ed0815de67857f48ef60
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-05-04 16:51:08 -0300

    refatoração: two way data binding -> template driven form

--- src/app/clientes/cliente-inserir/cliente-inserir.component.ts
+++ src/app/clientes/cliente-inserir/cliente-inserir.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Cliente } from "../cliente.model";
 //import { stringify } from "querystring";
 
@@ -17,15 +18,38 @@ export class ClienteInserirComponent{
   fone: string;
   email: string;
 
-  onAdicionarCliente(){
-      //. construir um objeto cliente que contém nome, fone e e-mail
-        const cliente: Cliente = {
-          nome: this.nome,
-          fone: this.fone,
-          email: this.email,
-        }
-      //2. passar esse objeto como argumento para o métodod emit
-      this.clienteAdicionado.emit(cliente);
-  }
+  //v1
+//   onAdicionarCliente(form: NgForm){
+//       //console.log(form);
+//       const cliente: Cliente = {
+//         nome: form.value.nome
+//         fone: form.value.fone
+//         email: form.value.email
+//       }
+//       this.clienteAdicionado.emit(cliente)
 
+//   }
+// }
+
+//v2
+onAdicionarCliente(cliente: Cliente){
+    this.clienteAdicionado.emit(cliente)
+  }
 }
+
+
+// onAdicionarCliente(){
+
+//   this.clienteAdicionado.emit(cliente)
+
+//   //1. construir um objeto cliente que contém nome, fone e e-mail
+//     const cliente: Cliente = {
+//       nome: this.nome,
+//       fone: this.fone,
+//       email: this.email,
+//     }
+//   //2. passar esse objeto como argumento para o métodod emit
+//   this.clienteAdicionado.emit(cliente);
+// }
+
+// }
