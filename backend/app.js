dcda88550cb473672c4d45146f027b81ec2e8280
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-05-11 18:29:01 -0300

    apostila 03: fim da secao 2.2
    implementacao do endpoint para acesso da lista de clientes

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,40 @@
 const express = require ('express')
 const app = express()
 
-app.use((req, res, next)=>{
-  console.log("Chegou uma requisição")
-  next()
+
+// app.use((req, res, next)=>{
+//   console.log("Chegou uma requisição")
+//   next()
+// })
+
+const clientes = [
+  {
+      id: '1',
+      nome: 'José,',
+      fone: '123456',
+      email: 'jose@mail.com'
+
+  },
+  {
+    id: '2',
+    nome: 'Antonio,',
+    fone: '789456',
+    email: 'antonio@mail.com'
+  }
+
+]
+
+//localhost:3000/api/clientes
+app.use('/api/clientes', (req, res) =>{
+  res.status(200).json({
+    mensagem: "Tudo OK",
+    clientes: clientes
+  })
 })
 
-app.use((req, res) =>{
-  res.send('Hello from the back end (Express)')
-})
+//v1 da funcao
+// app.use('/api/clientes', (req, res) =>{
+//   res.send('Hello from the back end (Express)')
+// })
 
 module.exports = app
