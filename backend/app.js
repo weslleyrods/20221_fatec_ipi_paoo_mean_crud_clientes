347598bb241b9db03822840fe785043cc8e15788
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-06-01 16:23:07 -0300

    apostila 04: fim da apostila GET e POST funcionando junto ao MongoDB. dotenv configurado

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,34 @@
+require('dotenv').config()
 const express = require ('express')
 const cors  = require('cors')
 const Cliente = require('./models/cliente')
 const app = express()
+const mongoose = require('mongoose')
 let id = 3;
 
-//use serve para usa ruum middleware
+const {
+  MONGODB_USER,
+  MONGODB_PASSWORD,
+  MONGODB_HOST,
+  MONGODB_CLUSTER,
+  MONGODB_DATABASE,
+} = process.env
+
+//"use" serve para usar um middleware
 
 // app.use((req, res, next)=>{
 //   console.log("Chegou uma requisição")
 //   next()
 // })
+
+//mongodb+srv://weslleyrods:<password>@cluster0.gvdih.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.${MONGODB_HOST}.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`)
+.then(()=>{
+  console.log("Conexão OK")
+})
+.catch((e)=>{
+  console.log("Conexão não OK"+e);
+})
 
 app.use(cors())
 //req.body pode ser tratado como um objeto JSON
@@ -31,7 +50,6 @@ const clientes = [
   }
 ]
 
-
 // app.use((req,res,next)=>{
 //   console.log("oi");
 //   res.json()
@@ -39,9 +57,12 @@ const clientes = [
 
 //localhost:3000/api/clientes
 app.get('/api/clientes', (req, res) =>{
-  res.status(200).json({
-    mensagem: "Tudo OK",
-    clientes: clientes
+  Cliente.find().then(documents=>{
+    res.status(200).json({
+      mensagem: "Tudo OK",
+      //clientes: clientes
+      clientes: documents
+    })
   })
 })
 
@@ -49,8 +70,11 @@ app.get('/api/clientes', (req, res) =>{
 // app.use('/api/clientes', (req, res) =>{
 //   res.send('Hello from the back end (Express)')
 // })
+
 //localhost:3000/api/clientes
 app.post('/api/clientes', (req, res)=>{
+  //objeto cliente com os dados vindo da requisição
+  //exibir com um log e armazenar no mongo db
   const cliente = new Cliente({
     ...req.body,
     //id: id++
@@ -61,6 +85,7 @@ app.post('/api/clientes', (req, res)=>{
   //   id: id++
   // })
   //res.end() encerra a resposta da requisição
+  cliente.save()
   res.status(201).json({mensagem: 'Cliente inserido'})
 })
 
